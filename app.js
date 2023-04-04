808b312529b1c75e3a62a674507bc50103ba6d1c
Camden Flater <crflater@wisc.edu>
2023-04-04 15:55:32 -0500

    new1

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ fetch("https://jsonplaceholder.typicode.com/comments")
   });
 
 //   2.2
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://jsonplaceholder.typicode.com/photos")
   .then((response) => response.json())
   .then((photos) => {
     console.log(photos.length);
@@ -39,5 +39,7 @@ fetch("https://jsonplaceholder.typicode.com/users")
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((users) => {
-    console.log(users.length);
+    users.forEach((user) => {
+      console.log(user.name + ", " + user.email);
+    });
   });
