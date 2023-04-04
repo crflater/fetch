b7221612e67ec220951bc7579038ed2252276214
Camden Flater <crflater@wisc.edu>
2023-04-04 15:44:19 -0500

    new

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-fetch("https://api.github.com/users/crflater").then((response) =>
-  console.log(response.json())
-);
+// 1.1
+fetch("https://api.github.com/users/crflater")
+  .then((response) => response.json())
+  .then((crflater) => {
+    console.log(crflater.public_repos);
+  });
+
+// 1.2
+fetch("https://api.github.com/users/crflater")
+  .then((response) => response.json())
+  .then((crflater) => {
+    console.log(crflater.followers);
+  });
+
+// 1.3
+fetch("https://api.github.com/users/crflater/followers")
+  .then((response) => response.json())
+  .then((crflater) => {
+    crflater.forEach((follower) => {
+      console.log(follower.id);
+    });
+  });
+
+//   2.1
+fetch("https://jsonplaceholder.typicode.com/comments")
+  .then((response) => response.json())
+  .then((comments) => {
+    console.log(comments.length);
+  });
+
+//   2.2
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((photos) => {
+    console.log(photos.length);
+  });
+
+//   2.3
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    console.log(users.length);
+  });
